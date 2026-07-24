bea1709843151d77da6837f65ad88f43902c3f80
MartinaEngler <martinafrank@bluewin.ch>
2026-07-24 15:56:31 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,1 @@
-document.querySelectorAll('input[type=checkbox]').forEach((c,i)=>{
-const k='camp_'+i;
-c.checked=localStorage.getItem(k)==='1';
-c.addEventListener('change',()=>localStorage.setItem(k,c.checked?'1':'0'));
-});
+document.querySelectorAll("input").forEach((c,i)=>{const k="camp"+i;c.checked=localStorage.getItem(k)=="1";c.onchange=()=>localStorage.setItem(k,c.checked?"1":"0");});
